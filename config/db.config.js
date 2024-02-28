1e30ca738f5c4ab45532dfa2d5cfd829ee6d9f22
Rafael Santana <rsantana@colum.edu>
2024-02-28 13:59:47 -0600

    heroku

--- config/db.config.js
+++ config/db.config.js
@@ -1,8 +1,8 @@
 module.exports = {
-    HOST: "localhost",
-    USER: "postgres",
-    PASSWORD: "7232",
-    DB: "serv_appointments",
+    HOST: "ec2-35-169-9-79.compute-1.amazonaws.com",
+    USER: "aslfuzkthpybam",
+    PASSWORD: "6eb12d3058cf30f015e9223e0fe9eac04e86c9e2490326debc3ea4ccdace6a6e",
+    DB: "dfk8amcv4a37d6",
     dialect: "postgres",
     pool: {
         max: 10,
