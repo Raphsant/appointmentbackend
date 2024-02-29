8692137d125907da75a45dff619d0fc9bf37efcb
Rafael Santana <rsantana@colum.edu>
2024-02-29 13:14:21 -0600

    Update database config to use environment variables and add Twilio library
    Updated the database configuration in db.config.js to use environment variables instead of hardcoded values, enhancing security. The Twilio library has also been added to package.json, enabling new communication features in our application.

--- config/db.config.js
+++ config/db.config.js
@@ -1,11 +1,11 @@
 module.exports = {
-    HOST: "ec2-35-169-9-79.compute-1.amazonaws.com",
-    USER: "aslfuzkthpybam",
-    PASSWORD: "6eb12d3058cf30f015e9223e0fe9eac04e86c9e2490326debc3ea4ccdace6a6e",
-    DB: "dfk8amcv4a37d6",
+    HOST: process.env.HOST,
+    USER: process.env.USER,
+    PASSWORD: process.env.PASSWORD,
+    DB: process.env.DATABASE,
     dialect: "postgres",
     host: "5432",
-    URL: "postgres://aslfuzkthpybam:6eb12d3058cf30f015e9223e0fe9eac04e86c9e2490326debc3ea4ccdace6a6e@ec2-35-169-9-79.compute-1.amazonaws.com:5432/dfk8amcv4a37d6",
+    URL: process.env.DATABASE_URL,
     pool: {
         max: 10,
         min: 0,
