1a6c27c0af978dd24050fdf447c6dddaefc49113
Rafael Santana <rsantana@colum.edu>
2024-03-01 13:18:46 -0600

    Update CORS origins and implement Twilio messaging
    CORS origins has been updated in server.js to include new domain. Alongside this, Twilio library has been integrated for sending automated messages, thus enhancing the application's communication capabilities.

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const Appointment = db.appointment;
 const app = express();
 
 let corsOptions = {
-  origin: ["https://sistemadecitas.netlify.app", "http://localhost:3000"]
+  origin: ["https://medbook-vistacentro.netlify.app", "http://localhost:3000"]
 };
 
 app.use(cors(corsOptions));
@@ -39,7 +39,20 @@ app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
 
-db.sequelize.sync().then(() => {
-  console.log("Drop and Resync Db");
+// db.sequelize.sync().then(() => {
+//   console.log("Drop and Resync Db");
+//
+// });
 
-});
+
+const accountSid = 'AC1947373f2ca9f65ba9639ba8a8225374';
+const authToken = 'd1df471b4c7c4b23f3cf99622ae0f731';
+const client = require('twilio')(accountSid, authToken);
+
+client.messages
+    .create({
+      body: 'El centro clinico vista centro le informa que su cita fue agendada,',
+      messagingServiceSid: 'MGf95103e4cda1035962adcfc275e47d7d',
+      to: '+18152950339'
+    })
+    .then(message => console.log(message.sid))
