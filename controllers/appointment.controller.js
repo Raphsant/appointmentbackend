008b257eaf0483fca6c7a8f2c283c902e38e6b0c
Rafael Santana <rsantana@colum.edu>
2024-03-18 15:21:08 -0500

    misc

--- controllers/appointment.controller.js
+++ controllers/appointment.controller.js
@@ -4,6 +4,23 @@ const sendAppointmentCreatedMessage = require("../whatsapp/aptmade");
 const Appointment = db.appointment;
 const User = db.user
 const Doctor = db.doctor
+const accountSid = process.env.ACCOUNTSID
+const authToken = process.env.AUTHTOKEN
+const client = require('twilio')(accountSid, authToken);
+function sendAppointmentCreatedMessage(dateAndTime, phone){
+    client.messages
+        .create({
+            messagingServiceSid: 'MGf95103e4cda1035962adcfc275e47d7d',
+            contentSid: 'HX444e87f5b0fa719e7a4ac9bf9af8f0d3',
+            from: 'whatsapp:+18777310396',
+            contentVariables: JSON.stringify({
+                1: dateAndTime,
+                2: ''
+            }),
+            to:  `whatsapp:${phone}`
+        })
+        .then(message => console.log(message.sid));
+}
 
 exports.createAppointment = async (req, res) => {
     try {
