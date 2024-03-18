88970da9338b805b12bcdf6047cebd819c29808b
Rafael Santana <rsantana@colum.edu>
2024-03-18 15:34:39 -0500

    misc

--- controllers/appointment.controller.js
+++ controllers/appointment.controller.js
@@ -6,15 +6,33 @@ const Doctor = db.doctor
 const accountSid = process.env.ACCOUNTSID
 const authToken = process.env.AUTHTOKEN
 const client = require('twilio')(accountSid, authToken);
+function formatDateAndTimeForMsg(dateTimeString) {
+    const date = new Date(dateTimeString);
+
+    // Options for formatting the date in Spanish
+    const dateOptions = { weekday: 'long', day: 'numeric', month: 'long' };
+    const timeOptions = { hour: 'numeric', minute: 'numeric', hour12: true };
+
+    // Formatters
+    const dateFormatter = new Intl.DateTimeFormat('es-ES', dateOptions);
+    const timeFormatter = new Intl.DateTimeFormat('es-ES', timeOptions);
+
+    // Formatting the date and time
+    const formattedDate = dateFormatter.format(date);
+    const formattedTime = timeFormatter.format(date);
+
+    return [formattedDate, formattedTime];
+}
 function sendAppointmentCreatedMessage(dateAndTime, phone){
+    const [date,time] = formatDateAndTimeForMsg()
     client.messages
         .create({
             messagingServiceSid: 'MGf95103e4cda1035962adcfc275e47d7d',
             contentSid: 'HX444e87f5b0fa719e7a4ac9bf9af8f0d3',
             from: 'whatsapp:+18777310396',
             contentVariables: JSON.stringify({
-                1: dateAndTime,
-                2: '12pm'
+                1: date,
+                2: time,
             }),
             to:  `whatsapp:${phone}`
         })
