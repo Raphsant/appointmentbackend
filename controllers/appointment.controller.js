b552ea99e90962fa629d43996105f8b89c25be26
Rafael Santana <rsantana@colum.edu>
2024-04-09 13:22:19 -0500

    Add elevate user functionality in user routes and controller
    Added a new route '/api/user/elevate' to provide admin privileges to a user in user.routes.js. Implemented the functionality to add 'admin' role to the user if it's not already assigned in user.controller.js. Further refactoring includes better error handling and improved code structure.

--- controllers/appointment.controller.js
+++ controllers/appointment.controller.js
@@ -4,45 +4,45 @@ const db = require("../models");
 const Appointment = db.appointment;
 const User = db.user
 const Doctor = db.doctor
-// const accountSid = process.env.ACCOUNTSID
-// const authToken = process.env.AUTHTOKEN
-// const client = require('twilio')(accountSid, authToken);
-// function formatDateAndTimeForMsg(dateTimeString) {
-//     const date = new Date(dateTimeString);
-//
-//     // Options for formatting the date in Spanish
-//     const dateOptions = { weekday: 'long', day: 'numeric', month: 'long' };
-//     const timeOptions = { hour: 'numeric', minute: 'numeric', hour12: true };
-//
-//     // Formatters
-//     const dateFormatter = new Intl.DateTimeFormat('es-ES', dateOptions);
-//     const timeFormatter = new Intl.DateTimeFormat('es-ES', timeOptions);
-//
-//     // Formatting the date and time
-//     const formattedDate = dateFormatter.format(date);
-//     const formattedTime = timeFormatter.format(date);
-//
-//     return [formattedDate, formattedTime];
-//}
-// function sendAppointmentCreatedMessage(dateAndTime, phone){
-//     try{
-//         // const [date,time] = formatDateAndTimeForMsg(dateAndTime)
-//         client.messages
-//             .create({
-//                 messagingServiceSid: 'MGf95103e4cda1035962adcfc275e47d7d',
-//                 contentSid: 'HX444e87f5b0fa719e7a4ac9bf9af8f0d3',
-//                 from: 'whatsapp:+18777310396',
-//                 contentVariables: JSON.stringify({
-//                     1: date,
-//                     2: time,
-//                 }),
-//                 to:  `whatsapp:${phone}`
-//             })
-//             .then(message => console.log(message?.sid));
-//     }catch (e){
-//         console.error(e.message)
-//     }
-// }
+const accountSid = process.env.ACCOUNTSID
+const authToken = process.env.AUTHTOKEN
+const client = require('twilio')(accountSid, authToken);
+function formatDateAndTimeForMsg(dateTimeString) {
+    const date = new Date(dateTimeString);
+
+    // Options for formatting the date in Spanish
+    const dateOptions = { weekday: 'long', day: 'numeric', month: 'long' };
+    const timeOptions = { hour: 'numeric', minute: 'numeric', hour12: true };
+
+    // Formatters
+    const dateFormatter = new Intl.DateTimeFormat('es-ES', dateOptions);
+    const timeFormatter = new Intl.DateTimeFormat('es-ES', timeOptions);
+
+    // Formatting the date and time
+    const formattedDate = dateFormatter.format(date);
+    const formattedTime = timeFormatter.format(date);
+
+    return [formattedDate, formattedTime];
+}
+function sendAppointmentCreatedMessage(dateAndTime, phone){
+    try{
+        const [date,time] = formatDateAndTimeForMsg(dateAndTime)
+        client.messages
+            .create({
+                messagingServiceSid: 'MGf95103e4cda1035962adcfc275e47d7d',
+                contentSid: 'HX444e87f5b0fa719e7a4ac9bf9af8f0d3',
+                from: 'whatsapp:+18777310396',
+                contentVariables: JSON.stringify({
+                    1: date,
+                    2: time,
+                }),
+                to:  `whatsapp:${phone}`
+            })
+            .then(message => console.log(message?.sid));
+    }catch (e){
+        console.error(e.message)
+    }
+}
 
 async function isUserAdmin(req, roles) {
     const triggerUser = await User.findByPk(req.userId)
@@ -82,14 +82,14 @@ exports.createAppointment = async (request, response) => {
         const {doctorId, userId, dateTime: dateTimeString} = request.body;
         const dateTime = new Date(dateTimeString);
         const isConfirmed = false
-
+        const targetUser = await User.findByPk(userId)
         const newAppointment = await appointmentService.createAppointment(
             doctorId,
             dateTime,
             userId,
             isConfirmed
         );
-
+        sendAppointmentCreatedMessage(dateTime, targetUser.phone)
         response.status(201).json(newAppointment);
     } catch (e) {
         handleError(e, response);
