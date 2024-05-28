a3298b2908f7b22762bbe5b072f2c9f907ff5fd7
Rafael Santana <rsantana@colum.edu>
2024-05-28 15:19:15 -0500

    Remove insurance related migration files and update user-insurance relation
    The migration files '20240520155846-insurance.js' and '20240520162627-insurance_id.js' have been deleted. The user-insurance relation in 'models/index.js' has been updated from 'belongsTo' to 'belongsToMany' allowing multiple insurances to be associated with a single user and vice-versa.

---
+++ migrations/20240528201856-fix-everything.js
@@ -0,0 +1,47 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.createTable('user_insurances', {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        allowNull: false,
+        autoIncrement: true,
+      },
+      userId: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'users',
+          key: 'id'
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE'
+      },
+      insuranceId: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'insurances',
+          key: 'id'
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE'
+      },
+      createdAt: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.NOW
+      },
+      updatedAt: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.NOW
+      }
+    });
+  },
+
+  async down (queryInterface, Sequelize) {
+    await queryInterface.dropTable('user_insurances');
+  }
+};
