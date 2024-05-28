140e42a0d15631be6402c6ce6c3724a03c7fec98
Rafael Santana <rsantana@colum.edu>
2024-05-28 15:16:27 -0500

    Remove insurance related migration files and update user-insurance relation
    The migration files '20240520155846-insurance.js' and '20240520162627-insurance_id.js' have been deleted. The user-insurance relation in 'models/index.js' has been updated from 'belongsTo' to 'belongsToMany' allowing multiple insurances to be associated with a single user and vice-versa.

--- migrations/20240528195848-create-insurances-table.js
+++ migrations/20240528195848-create-insurances-table.js
@@ -3,18 +3,45 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
-    await queryInterface.addColumn('users', 'insuranceId', {
-      type: Sequelize.INTEGER,
-      references: {
-        model: 'insurances', // Name of the table the foreign key references
-        key: 'id'
+    await queryInterface.createTable('user_insurances', {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        allowNull: false,
+        autoIncrement: true,
       },
-      onUpdate: 'CASCADE',
-      onDelete: 'SET NULL'
+      userId: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'users',
+          key: 'id'
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE'
+      },
+      insuranceId: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'insurances',
+          key: 'id'
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE'
+      },
+      createdAt: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.NOW
+      },
+      updatedAt: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.NOW
+      }
     });
   },
 
   async down (queryInterface, Sequelize) {
-    await queryInterface.removeColumn('users', 'insuranceId');
+    await queryInterface.dropTable('user_insurances');
   }
 };
