19d99d4d9da424781eee34bcd8192d9415593d31
Rafael Santana <rsantana@colum.edu>
2024-06-27 16:20:04 -0500

    Add Access-Control-Allow-Origin in headers
    The "Access-Control-Allow-Origin" header has been added in all routing files. This update broadens the scope of Access-Control-Allow-Headers to include the origin of the request, thereby improving functionality and versatility in handling cross-origin requests.

--- routes/doctor.routes.js
+++ routes/doctor.routes.js
@@ -1,26 +1,22 @@
 const controller = require("../controllers/doctor.controller");
-const {authJwt} = require("../middleware");
-const origen = ["http://localhost:3000", "http://10.0.0.121:3000", "https://vistacentro-citas.com", "https://seguros.vistacentro-citas.com/"]
+const { authJwt } = require("../middleware");
 
 module.exports = function (app) {
-    app.use(function(req, res, next) {
-        // Allow requests from any origin
-        res.header("Access-Control-Allow-Origin", origin);
+  app.use(function (req, res, next) {
+    res.header(
+      "Access-Control-Allow-Headers",
+      "x-access-token, Origin, Content-Type, Accept"
+    );
+    next();
+  });
 
-        // Allow specific headers for CORS requests
-        res.header("Access-Control-Allow-Headers",
-            "x-access-token, Origin, Content-Type, Accept");
-
-        next();
-    });
-
-    app.post("/api/doctor/create", authJwt.verifyToken, controller.createDoctor);
-    app.post("/api/doctor/edit", authJwt.verifyToken, controller.editDoctor);
-    app.post("/api/doctor/delete", authJwt.verifyToken, controller.deleteDoctor);
-    app.get("/api/doctor", authJwt.verifyToken, controller.getAllDoctors);
-    app.get("/api/getDoctor", authJwt.verifyToken, controller.getDoctor);
-    app.get("/api/doctor/count", authJwt.verifyToken, controller.doctorCount)
-    app.get("/api/doctor/specialty", controller.getDoctorBySpecialty)
+  app.post("/api/doctor/create", authJwt.verifyToken, controller.createDoctor);
+  app.post("/api/doctor/edit", authJwt.verifyToken, controller.editDoctor);
+  app.post("/api/doctor/delete", authJwt.verifyToken, controller.deleteDoctor);
+  app.get("/api/doctor", authJwt.verifyToken, controller.getAllDoctors);
+  app.get("/api/getDoctor", authJwt.verifyToken, controller.getDoctor);
+  app.get("/api/doctor/count", authJwt.verifyToken, controller.doctorCount)
+  app.get("/api/doctor/specialty", controller.getDoctorBySpecialty)
 };
 
 
