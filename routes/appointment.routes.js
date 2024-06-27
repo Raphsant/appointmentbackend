58703f9607d6c1f4a8d17481144771b4a73387d4
Rafael Santana <rsantana@colum.edu>
2024-06-27 16:16:52 -0500

    Add Access-Control-Allow-Origin in headers
    The "Access-Control-Allow-Origin" header has been added in all routing files. This update broadens the scope of Access-Control-Allow-Headers to include the origin of the request, thereby improving functionality and versatility in handling cross-origin requests.

--- routes/appointment.routes.js
+++ routes/appointment.routes.js
@@ -1,13 +1,16 @@
 const controller = require("../controllers/appointment.controller");
 const {authJwt} = require("../middleware");
+const origen = ["http://localhost:3000", "http://10.0.0.121:3000", "https://vistacentro-citas.com", "https://seguros.vistacentro-citas.com/"]
 
 module.exports = function (app) {
-    app.use(function (req, res, next) {
-        res.header(
-            "Access-Control-Allow-Headers",
-            "Access-Control-Allow-Origin",
-            "x-access-token, Origin, Content-Type, Accept"
-        );
+    app.use(function(req, res, next) {
+        // Allow requests from any origin
+        res.header("Access-Control-Allow-Origin", origin);
+
+        // Allow specific headers for CORS requests
+        res.header("Access-Control-Allow-Headers",
+            "x-access-token, Origin, Content-Type, Accept");
+
         next();
     });
 
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,13 +1,16 @@
 const {authJwt} = require("../middleware");
 const controller = require("../controllers/user.controller");
+const origen = ["http://localhost:3000", "http://10.0.0.121:3000", "https://vistacentro-citas.com", "https://seguros.vistacentro-citas.com/"]
 
 module.exports = function (app) {
-    app.use(function (req, res, next) {
-        res.header(
-            "Access-Control-Allow-Headers",
-            "Access-Control-Allow-Origin",
-            "x-access-token, Origin, Content-Type, Accept"
-        );
+    app.use(function(req, res, next) {
+        // Allow requests from any origin
+        res.header("Access-Control-Allow-Origin", origin);
+
+        // Allow specific headers for CORS requests
+        res.header("Access-Control-Allow-Headers",
+            "x-access-token, Origin, Content-Type, Accept");
+
         next();
     });
 
